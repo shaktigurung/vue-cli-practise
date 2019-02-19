58a1e0391653d394c41bb4dfe68e8e6830fc8fcc
shaktigurung <mr.shakti.gurung@gmail.com>
2019-02-19 15:19:44 +1100

    cli practices

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>CLI</span>
+        <span class="font-weight-light">WEB DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
